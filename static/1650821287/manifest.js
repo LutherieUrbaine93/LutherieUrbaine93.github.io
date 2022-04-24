03d8b85401f7c6ca6d0ec3c4ce95d1809c17ff24
LutherieUrbaine93 <adrientasic7@gmail.com>
2022-04-24 17:28:34 +0000

    Update from https://github.com/LutherieUrbaine93/nuxt/commit/d290cb1f0dd91750c90b8effb1fbf6c66c17353a

---
+++ static/1650821287/manifest.js
@@ -0,0 +1,1 @@
+__NUXT_JSONP__("manifest.js", {routes:["\u002Fsignup","\u002F","\u002Factivites","\u002Fpedagogie-de-la-creation","\u002Fateliers-stages","\u002Fpresse","\u002Fecouter-voir","\u002Factualites","\u002Finstrumentarium","\u002Fcreations-tous-terrains"]})
